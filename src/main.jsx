7ea96797f7d0ecafcf5e8e5099cff3f217aee864
Michael Drummond <michael.drummond328@gmail.com>
2022-07-05 14:14:47 -0600

    forgot to uncomment React.StrictMode after testing

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,11 @@ import './index.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <BrowserRouter>
       <CandidateContextProvider>
         <App />
       </CandidateContextProvider>
     </BrowserRouter>
-  // </React.StrictMode>
+  </React.StrictMode>
 )
